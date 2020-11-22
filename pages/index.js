ca7e4d9e569e0fb3dbdf2cb48d50d0dab8a200a0
Jonas Witt <mail@jonaswitt.com>
2020-11-22 15:38:21 +0000

    prettier

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import Head from 'next/head'
-import Calculator from '../src/components/Calculator'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Calculator from "../src/components/Calculator";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Pizza Calculator</title>
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Pizza Calculator</title>
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to the Pizza Calculator
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome to the Pizza Calculator</h1>
 
-        <Calculator />
-
-      </main>
-    </div>
-  )
+                <Calculator />
+            </main>
+        </div>
+    );
 }
