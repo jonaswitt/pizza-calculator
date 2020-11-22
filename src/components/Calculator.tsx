868b303059e64f9a1211943673acf784a50b4676
Jonas Witt <mail@jonaswitt.com>
2020-11-22 18:04:51 +0100

    persist form state in query string for URL sharing

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,15 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
 
 type FormValues = {
     doughBallCount: number;
     doughBallSizeGrams: number;
 };
+type QueryStringValues = {
+    [P in keyof FormValues]?: string;
+};
+
+const DEFAULT_VALUES: FormValues = {
+    doughBallCount: 1,
+    doughBallSizeGrams: 250,
+};
+
+const deserializeValues = (qs?: string): Partial<FormValues> => {
+    const query = queryString.parse(qs) as QueryStringValues;
+    const values: Partial<FormValues> = {};
+    for (const key in query) {
+        const value = Number(query[key]);
+        if (!Number.isNaN(value)) {
+            values[key] = value;
+        }
+    }
+    return values;
+};
+
+const loadValuesQs = (): Partial<FormValues> => deserializeValues(window?.location.search);
+
+const serializeValues = (values: FormValues): string => queryString.stringify(values);
+
+const storeValuesQs = (values: FormValues) => {
+    window?.history.replaceState(null, null, "?" + serializeValues(values));
+};
 
 const Calculator: React.FC = () => {
-    const [values, setValues] = useState<FormValues>({
-        doughBallCount: 1,
-        doughBallSizeGrams: 250,
-    });
+    const [values, setValues] = useState<FormValues>(DEFAULT_VALUES);
+
+    // Load/store form values from/in query string
+    useEffect(() => {
+        const qsValues = loadValuesQs();
+        setValues((values) => ({
+            ...values,
+            ...qsValues,
+        }));
+    }, [setValues]);
+    useEffect(() => {
+        storeValuesQs(values);
+    }, [values]);
 
     return (
         <div>
