c2e60249556a6bb4729db6f78f572f768d3cbe2b
Jonas Witt <mail@jonaswitt.com>
2020-11-22 18:35:46 +0100

    extend form values; calculate yeast amount using rafcalc method

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,16 +2,26 @@ import React, { useEffect, useState } from "react";
 import queryString from "query-string";
 
 type FormValues = {
-    doughBallCount: number;
-    doughBallSizeGrams: number;
+    ballCount: number;
+    ballWeightGrams: number;
+    hydrationPerc: number;
+    levitationTemperatureC: number;
+    levitationTimeHrs: number;
+    saltGpl: number;
+    oilGpl: number;
 };
 type QueryStringValues = {
     [P in keyof FormValues]?: string;
 };
 
 const DEFAULT_VALUES: FormValues = {
-    doughBallCount: 1,
-    doughBallSizeGrams: 250,
+    ballCount: 4,
+    ballWeightGrams: 250,
+    hydrationPerc: 65,
+    levitationTemperatureC: 20,
+    levitationTimeHrs: 6,
+    saltGpl: 40,
+    oilGpl: 0,
 };
 
 const deserializeValues = (qs?: string): Partial<FormValues> => {
@@ -49,25 +59,95 @@ const Calculator: React.FC = () => {
         storeValuesQs(values);
     }, [values]);
 
+    const flourRelativeWeight = 1;
+    const waterRelativeWeight = values.hydrationPerc / 100;
+    const saltRelativeWeight = (values.saltGpl / 1000) * waterRelativeWeight;
+    const oilRelativeWeight = (values.oilGpl / 1000) * waterRelativeWeight;
+    const totalRelativeWeight = waterRelativeWeight + flourRelativeWeight + saltRelativeWeight + oilRelativeWeight;
+
+    const totalWeight = values.ballCount * values.ballWeightGrams;
+    const flourWeight = totalWeight * (flourRelativeWeight / totalRelativeWeight);
+    const waterWeight = totalWeight * (waterRelativeWeight / totalRelativeWeight);
+    const saltWeight = totalWeight * (saltRelativeWeight / totalRelativeWeight);
+    const oilWeight = totalWeight * (oilRelativeWeight / totalRelativeWeight);
+
+    // https://pizzanapo.fr/topic/3-rafcalc/ -- Japi2
+    const yeastWeight =
+        (flourWeight * 2250 * (1 + values.saltGpl / 200) * (1 + values.oilGpl / 300)) /
+        ((-80 + 4.2 * values.hydrationPerc - 0.0305 * values.hydrationPerc ** 2) *
+            values.levitationTemperatureC ** 2.5 *
+            values.levitationTimeHrs ** 1.2);
+
     return (
         <div>
             <div>
                 Dough Balls:
                 <input
-                    value={values.doughBallCount}
-                    onChange={(e) => setValues((values) => ({ ...values, doughBallCount: Number(e.target.value) }))}
+                    value={values.ballCount}
+                    onChange={(e) => setValues((values) => ({ ...values, ballCount: Number(e.target.value) }))}
                 />
             </div>
 
             <div>
-                Dough Ball Size (g):
+                Dough Ball Weight (g):
                 <input
-                    value={values.doughBallSizeGrams}
-                    onChange={(e) => setValues((values) => ({ ...values, doughBallSizeGrams: Number(e.target.value) }))}
+                    value={values.ballWeightGrams}
+                    onChange={(e) => setValues((values) => ({ ...values, ballWeightGrams: Number(e.target.value) }))}
                 />
             </div>
 
-            <div>Dough (g): {values.doughBallCount * values.doughBallSizeGrams}</div>
+            <div>Dough Total (g): {totalWeight}</div>
+
+            <div>
+                Hydration (%):
+                <input
+                    value={values.hydrationPerc}
+                    onChange={(e) => setValues((values) => ({ ...values, hydrationPerc: Number(e.target.value) }))}
+                />
+            </div>
+
+            <div>Flour (g): {flourWeight.toFixed(0)}</div>
+            <div>Water (g): {waterWeight.toFixed(0)}</div>
+
+            <div>
+                Salt (Grams per Liter):
+                <input
+                    value={values.saltGpl}
+                    onChange={(e) => setValues((values) => ({ ...values, saltGpl: Number(e.target.value) }))}
+                />
+            </div>
+
+            <div>
+                Oil (Grams per Liter):
+                <input
+                    value={values.oilGpl}
+                    onChange={(e) => setValues((values) => ({ ...values, oilGpl: Number(e.target.value) }))}
+                />
+            </div>
+            <div>Salt (g): {saltWeight.toFixed(1)}</div>
+            <div>Oil (g): {oilWeight.toFixed(1)}</div>
+
+            <div>
+                Levitation Temperature (C):
+                <input
+                    value={values.levitationTemperatureC}
+                    onChange={(e) =>
+                        setValues((values) => ({ ...values, levitationTemperatureC: Number(e.target.value) }))
+                    }
+                />
+            </div>
+
+            <div>
+                Levitation Time (h):
+                <input
+                    value={values.levitationTimeHrs}
+                    onChange={(e) => setValues((values) => ({ ...values, levitationTimeHrs: Number(e.target.value) }))}
+                />
+            </div>
+
+            <div>Fresh Yeast (g): {yeastWeight.toFixed(1)}</div>
+
+            <button onClick={() => setValues(DEFAULT_VALUES)}>Reset Values</button>
         </div>
     );
 };
